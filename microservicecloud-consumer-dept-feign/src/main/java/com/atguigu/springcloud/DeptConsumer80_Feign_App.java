166e0e50761a23c2dba5f15ce962e5c7ea82d5f8
563143188@qq.com <563143188@qq.com>
2020-03-16 11:12:41 +0800

    调整了版本依赖问题

--- microservicecloud-consumer-dept-feign/src/main/java/com/atguigu/springcloud/DeptConsumer80_Feign_App.java
+++ microservicecloud-consumer-dept-feign/src/main/java/com/atguigu/springcloud/DeptConsumer80_Feign_App.java
@@ -3,8 +3,9 @@ package com.atguigu.springcloud;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
-import org.springframework.cloud.netflix.feign.EnableFeignClients;
+import org.springframework.cloud.openfeign.EnableFeignClients;
 import org.springframework.context.annotation.ComponentScan;
+
 
 @SpringBootApplication
 @EnableEurekaClient
